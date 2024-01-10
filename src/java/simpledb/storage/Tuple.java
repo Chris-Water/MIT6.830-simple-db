0a19098e6f7c04d37ffe40d74a6b48c9c328f97c
Chris Water <zzzhhhqqq123@gmail.com>
2024-01-10 22:56:54 +0800

    lab1 tuple

--- src/java/simpledb/storage/Tuple.java
+++ src/java/simpledb/storage/Tuple.java
@@ -1,8 +1,10 @@
 package simpledb.storage;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Tuple maintains information about the contents of a tuple. Tuples have a
@@ -11,19 +13,26 @@ import java.util.List;
  */
 public class Tuple implements Serializable {
 
-    List<Field> fields;
+    private List<Field> fields;
 
+    private TupleDesc tupleDesc;
+
+    private RecordId recordId;
     private static final long serialVersionUID = 1L;
 
     /**
      * Create a new tuple with the specified schema (type).
      *
-     * @param td
-     *            the schema of this tuple. It must be a valid TupleDesc
-     *            instance with at least one field.
+     * @param td the schema of this tuple. It must be a valid TupleDesc
+     *           instance with at least one field.
      */
     public Tuple(TupleDesc td) {
-        // some code goes here
+        // 根据描述创建一行数据
+        tupleDesc = td;
+        fields = new ArrayList<>(td.numFields());
+        for (int i = 0; i <td.numFields() ; i++) {
+            fields.add(null);
+        }
     }
 
     /**
@@ -31,79 +40,76 @@ public class Tuple implements Serializable {
      */
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return tupleDesc;
     }
 
     /**
      * @return The RecordId representing the location of this tuple on disk. May
-     *         be null.
+     * be null.
      */
     public RecordId getRecordId() {
         // some code goes here
-        return null;
+        return recordId;
     }
 
     /**
      * Set the RecordId information for this tuple.
      *
-     * @param rid
-     *            the new RecordId for this tuple.
+     * @param rid the new RecordId for this tuple.
      */
     public void setRecordId(RecordId rid) {
         // some code goes here
+        recordId = rid;
     }
 
     /**
      * Change the value of the ith field of this tuple.
      *
-     * @param i
-     *            index of the field to change. It must be a valid index.
-     * @param f
-     *            new value for the field.
+     * @param i index of the field to change. It must be a valid index.
+     * @param f new value for the field.
      */
     public void setField(int i, Field f) {
-        // some code goes here
+        // 校验index
+        if (i < 0 || i >= fields.size()) throw new IllegalArgumentException("invalid index");
+        fields.set(i, f);
     }
 
     /**
+     * @param i field index to return. Must be a valid index.
      * @return the value of the ith field, or null if it has not been set.
-     *
-     * @param i
-     *            field index to return. Must be a valid index.
      */
     public Field getField(int i) {
         // some code goes here
-        return null;
+        if (i < 0 || i >= fields.size()) throw new IllegalArgumentException("invalid index");
+        return fields.get(i);
     }
 
     /**
      * Returns the contents of this Tuple as a string. Note that to pass the
      * system tests, the format needs to be as follows:
-     *
+     * <p>
      * column1\tcolumn2\tcolumn3\t...\tcolumnN
-     *
+     * <p>
      * where \t is any whitespace (except a newline)
      */
     public String toString() {
         // some code goes here
-        throw new UnsupportedOperationException("Implement this");
+        return fields.stream().map(Field::toString).collect(Collectors.joining("\t"));
     }
 
     /**
-     * @return
-     *        An iterator which iterates over all the fields of this tuple
-     * */
-    public Iterator<Field> fields()
-    {
+     * @return An iterator which iterates over all the fields of this tuple
+     */
+    public Iterator<Field> fields() {
         // some code goes here
-        return null;
+        return fields.iterator();
     }
 
     /**
      * reset the TupleDesc of this tuple (only affecting the TupleDesc)
-     * */
-    public void resetTupleDesc(TupleDesc td)
-    {
+     */
+    public void resetTupleDesc(TupleDesc td) {
         // some code goes here
+        this.tupleDesc = td;
     }
 }
